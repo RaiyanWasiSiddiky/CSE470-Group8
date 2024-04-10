5d9b51960e715118a036d873cee72155b24f6b69
Raiyan Wasi Siddiky <raiyanwasisiddiky@gmail.com>
2024-04-10 06:07:45 +0600

    fixed admin user deletion

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -7,6 +7,7 @@ router.get('/authenticate', adminController.get_authenticate);
 
 router.post('/authenticate/accept', adminController.post_acceptApplicant);
 router.post('/authenticate/reject', adminController.post_rejectApplicant);
+
 router.delete('/:id/delete', adminController.delete_user);
 
 module.exports =router;
